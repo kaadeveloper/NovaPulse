d2fe5c305db17679fa86d51ffccd596637f9b030
kaadeveloper <zenojanssens3@gmail.com>
2026-06-23 17:36:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.post("/chat", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("NovaPulse AI draait op http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`NovaPulse AI draait op poort ${PORT}`);
 });
